2ee6abd7b8c6605d2717e23c1280ed70c182d8b6
Alex Shostka <burda3000@gmail.com>
2019-10-13 02:55:00 +0800

    HW_Lesson3

--- src/com/urise/webapp/storage/ArrayStorage.java
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -7,8 +7,10 @@ import java.util.Arrays;
 /**
  * Array based storage for Resumes
  */
-public class ArrayStorage {
-    private Resume[] storage = new Resume[10_000];
+public class ArrayStorage implements Storage {
+    private static final int STORAGE_LIMIT = 10_000;
+
+    private Resume[] storage = new Resume[STORAGE_LIMIT];
     private int count = 0;
 
     public void clear() {
