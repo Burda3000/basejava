baa8dbed137f53ec9b26ee6164ad83b2e4649985
Alex Shostka <burda3000@gmail.com>
2019-11-26 11:44:32 +0800

    HW_Lesson4

--- test/com/urise/webapp/storage/AbstractArrayStorageTest.java
+++ test/com/urise/webapp/storage/AbstractArrayStorageTest.java
@@ -33,31 +33,37 @@ public class AbstractArrayStorageTest {
     @Test
     public void clear() throws Exception {
         storage.clear();
+        Assert.assertEquals(0, storage.size());
     }
 
     @Test
     public void update() throws Exception {
-
+        Resume resume_1 = new Resume(UUID_1);
+        storage.update(resume_1);
+        Assert.assertSame(resume_1, storage.get(UUID_1));
     }
 
     @Test
     public void getAll() throws Exception {
-
+        storage.getAll();
     }
 
     @Test
     public void save() throws Exception {
-
+        Resume resume_4 = new Resume();
+        storage.save(resume_4);
+        Assert.assertEquals(4, storage.size());
     }
 
     @Test
     public void delete() throws Exception {
-
+        storage.delete(UUID_1);
+        Assert.assertEquals(2, storage.size());
     }
 
     @Test
     public void get() throws Exception {
-
+        Assert.assertEquals(UUID_1, storage.get(UUID_1));
     }
 
     @Test(expected = NotExistStorageException.class)
