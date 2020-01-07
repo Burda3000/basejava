825425fae6ef4990eee6baf236bb4800dc478c20
Alex Shostka <burda3000@gmail.com>
2020-01-07 14:47:11 +0800

    HW_Lesson7 - pre_HW

--- src/com/urise/webapp/storage/AbstractStorage.java
+++ src/com/urise/webapp/storage/AbstractStorage.java
@@ -6,30 +6,38 @@ import com.urise.webapp.model.Resume;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.logging.Logger;
 
 public abstract class AbstractStorage<SK> implements Storage {
 
+    private static final Logger LOG = Logger.getLogger(AbstractStorage.class.getName());
+
     public void update(Resume resume) {
+        LOG.info("Update " + resume);
         SK searchKey = getIndexNotExist(resume.getUuid());
         doUpdate(searchKey, resume);
     }
 
     public void save(Resume resume) {
+        LOG.info("Save " + resume);
         SK searchKey = getIndexExist(resume.getUuid());
         doSave(searchKey, resume);
     }
 
     public Resume get(String uuid) {
+        LOG.info("Get " + uuid);
         SK searchKey = getIndexNotExist(uuid);
         return doGet(searchKey);
     }
 
     public void delete(String uuid) {
+        LOG.info("Delete " + uuid);
         SK searchKey = getIndexNotExist(uuid);
         doDelete(searchKey);
     }
 
     public List<Resume> getAllSorted() {
+        LOG.info("get all sorted");
         List<Resume> list = getAllCopy();
         Collections.sort(list);
         return list;
@@ -40,6 +48,7 @@ public abstract class AbstractStorage<SK> implements Storage {
         if (isExist(searchKey)) {
             return searchKey;
         } else {
+            LOG.warning("Resume " + uuid + " already exist");
             throw new NotExistStorageException(uuid);
         }
     }
@@ -49,6 +58,7 @@ public abstract class AbstractStorage<SK> implements Storage {
         if (!isExist(searchKey)) {
             return searchKey;
         } else {
+            LOG.warning("Resume " + uuid + " not exist");
             throw new ExistStorageException(uuid);
         }
     }
